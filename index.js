3b1ed22350d9b31df8f1720048d7520ae6e2e9a9
Pratheek G Shetty <pratheekshetty934@gmail.com>
2025-03-29 13:11:59 +0530

    cors middleware added

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const cors=require('cors');
 const app=express();
-app.use(cors());
+app.use(cors({
+    origin:"*"
+}));
 require("dotenv").config();
 const port=process.env.PORT||8080;
 app.get('/',(req,res)=>{
