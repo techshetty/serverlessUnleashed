c7a89221c8523b96b2f6a654645f02a15c833a5b
Pratheek G Shetty <pratheekshetty934@gmail.com>
2025-03-31 16:56:35 +0530

    minor-changes

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(cors({
 }));
 require("dotenv").config();
 const port=process.env.PORT||8080;
+cmap={}
 app.get('/',(req,res)=>{
     return res.send("Welcome to Serverless Unleashed Workshop");
 })
@@ -17,6 +18,17 @@ app.get('/greet/:name',(req,res)=>{
     const name=req.params.name||"mate";
     return res.send(`Hello ${name}, nice to meet you.`);
 })
+app.get('/record',(req,res)=>{
+    const name=req.query.name;
+    if(!name) return res.json({msg:"Hello Mate, how are you?.Please provide me your name."})
+    if(cmap[name]){
+        return res.json({msg:cmap[name]});
+    }
+    else{
+        cmap[name]=`${name} called the api for the first time at ${new Date().toLocaleString()}`;
+        return res.json({msg:`${name} called the api for the first time.`})
+    }
+});
 app.listen(port,()=>{
 console.log(`Server running at port ${port}`);
 })
